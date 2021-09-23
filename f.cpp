83d819b6ee23e2e07559fcd2db5f83c6cfc58627
Toadsong8451 <gs20059@gs.hs.kr>
2021-09-23 15:04:24 +0900

    15:04

--- f.cpp
+++ f.cpp
@@ -3,17 +3,27 @@
 #include "lib.h"
 int main()
 {
-    int i;
-    scanf("%d", &i);
+    long long int i;
+    scanf("%lld", &i);
     long long int a=(f(i, 2)-2*f(i, 1)+f(i, 0))/2;
     long long int b=f(i, 1)-f(i, 0)-a;
     long long int c=f(i, 0);
-    int k1=-2*b/a;
-    int k2=k1+1;
-    int k3=k1-1;
-    long long int m=100000000000;
-    if(m>=a*k1*k1+b*k1+c) m=a*k1*k1+b*k1+c;
-    if(m>=a*k2*k2+b*k2+c) m=a*k2*k2+b*k2+c;
-    if(m>=a*k3*k3+b*k3+c) m=a*k3*k3+b*k3+c;
-    printf("%llu", m);
+    if(a==0)
+    {
+        if(b==0) printf("%lld", c);
+        else if(b>0) printf("%lld", f(i, -100000000));
+        else printf("%lld", f(i, 100000000));
+    }
+    else
+    {
+        long long int k1=-b/a/2;
+        long long int k2=k1+1;
+        long long int k3=k1-1;
+        long long int m=9223372036854775807;
+        if(m>=f(i, k1)) m=f(i, k1);
+        if(m>=f(i, k2)) m=f(i, k2);
+        if(m>=f(i, k3)) m=f(i, k3);
+        printf("%lld", m);
+    }
+    
 }
