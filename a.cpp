9f8c6bc37c5846785fb1f7fb7eaea06982136f11
Toadsong8451 <gs20059@gs.hs.kr>
2021-09-23 13:52:41 +0900

    first commit

--- a.cpp
+++ a.cpp
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <algorithm>
+using namespace std;
+int a[701];
 int main()
 {
-    printf("1");
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for(int i=1; i<=n; i++)
+    {
+        int x;
+        scanf("%d", &x);
+        a[x]++; //n-m개를 골라서 최소로
+    }
+    int k=n-m;
+    long long int ans=0;
+    for(int i=1; i<=700; i++)
+    {
+        int m=min(k, a[i]);
+        k-=m;
+        ans+=m*i;
+    }
+    printf("%lld", ans);
 }
