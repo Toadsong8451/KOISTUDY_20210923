798cd75fa00b1ed1b3138657123985206d30c45c
Toadsong8451 <90604899+Toadsong8451@users.noreply.github.com>
2021-09-24 23:13:43 +0900

    Update g.cpp

--- g.cpp
+++ g.cpp
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <algorithm>
 #include "lib.h"
+using namespace std;
 int main()
 {
+    int i=1;
+    int j=100;
     int k;
-    f()
+    scanf("%d", &k);
+    for(int i=1; i<=100; i++)
+    {
+        int p=-1;
+        for(;j>0; j--)
+        {
+            int t=function(i, j);
+            if(t==k) p=j;
+            if(t<k)
+            {
+                j=min(j+1, 100);
+                break;
+            }
+        }
+        if(p!=-1)
+        {
+            printf("%d %d", i, p);
+            return 0;
+        }
+        if(j==0) break;
+    }
+    printf("-1");
 }
