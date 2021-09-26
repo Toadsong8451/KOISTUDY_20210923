53bb89ccd053a93d061ae383bcb6b0fef14109d1
Toadsong8451 <90604899+Toadsong8451@users.noreply.github.com>
2021-09-26 18:04:34 +0900

    Update j.cpp

--- j.cpp
+++ j.cpp
@@ -1,39 +1,34 @@
-#include<cstdio>
-#include<queue>
+#include<stdio.h>
 #include<algorithm>
-#include<vector>
-#include<iostream>
 using namespace std;
-int N, K;
-int ans;
-int arr[1010];
-
+int n, k, ans;
+int a[1010];
 int dp[1010][310];
 bool cmp(int x, int y)
 {
     return x>y;
 }
-
 int main()
 {
-    scanf("%d %d", &N, &K);
-    for(int i=1; i<=N; i++) scanf("%d", arr+i);
-    sort(arr+1, arr+N+1, cmp);
-    for(int i=0; i<K; i++)
+    scanf("%d %d", &n, &k);
+    for(int i=1; i<=n; i++) scanf("%d", &a[i]);
+    sort(a+1, a+n+1, cmp);
+    for(int i=0; i<k; i++)
     {
         int x;
         scanf("%d", &x);
-        ans += x;
+        ans+=x;
     }
-
-    for(int i=0; i<=N; i++) for(int j=0; j<=K; j++) dp[i][j]= 3000000;
-
+    for(int i=0; i<=n; i++) for(int j=0; j<=k; j++) dp[i][j]= 3000000;
     dp[0][0]=0;
-    for(int i=1; i<=N; i++) for(int j=0; j<=K; j++)
+    for(int i=1; i<=n; i++)
     {
-        dp[i][j] = dp[i-1][j];
-        if((i-2)>3*(j-1) && i>=2 && j>=1) dp[i][j] = min(dp[i][j], dp[i-2][j-1]+arr[i-1] - arr[i]);
+        for(int j=0; j<=k; j++)
+        {
+            dp[i][j]=dp[i-1][j];
+            if((i-2)>3*(j-1)&&i>=2&&j>=1) dp[i][j]=min(dp[i][j], dp[i-2][j-1]+a[i-1]-a[i]);
+        }
     }
-    ans -= dp[N][K];
+    ans-=dp[n][k];
     printf("%d", ans);
 }
